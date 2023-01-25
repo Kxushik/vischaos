4cb10ea8d3d54906615e31984ba684d0a3177234
Kaushik Paul <kaushikpaul50@gmail.com>
2023-01-25 13:39:34 -0500

    Fps tracking

--- visual_chaos.py
+++ visual_chaos.py
@@ -53,6 +53,7 @@ def cube():
 
 
 def main():
+    clock = pygame.time.Clock()
     pygame.init()
     display = (800,600)
     pygame.display.set_mode(display, DOUBLEBUF|OPENGL)
@@ -60,7 +61,8 @@ def main():
     gluPerspective(45, (display[0]/display[1]), 0.1, 50.0)
 
     glTranslatef(0.0,0.0, -10)
-
+    max_fps = 0;
+    min_fps = 100;
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -68,10 +70,20 @@ def main():
                 quit()
 
         glRotatef(1, 3, 1, 1)
+        clock.tick()
         glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT)
         cube()
         pygame.display.flip()
         pygame.time.wait(10)
+        fps = clock.get_fps()
+        if fps>max_fps:
+            max_fps=fps
+        elif fps<min_fps:
+            min_fps=fps
+        print(fps)
+        print("MAX:"+str(max_fps))
+        print("MIN:"+str(min_fps))
+    
 
 if __name__ == "__main__":
     main()
